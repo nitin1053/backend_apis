fc07ff6ba35a4cd2385723f209cd3503eee13281
Nitin <nitinjha1053@gmail.com>
2024-05-20 12:23:58 +0530

    add

---
+++ app.js
@@ -0,0 +1,12 @@
+const express = require('express');
+const app = express();
+
+const port = 80;
+
+app.get('/sayHello', (req, res) => {
+    res.json({ message: "Hello User" });
+});
+
+app.listen(port, () => {
+    console.log(`API running on port ${port}`);
+});
